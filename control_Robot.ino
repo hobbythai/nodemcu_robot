457edd06219a9de1ff404ab581f12d5e2263fe13
kally <kally.thailand@gmail.com>
2016-10-09 14:01:35 +0700

    final

--- control_Robot.ino
+++ control_Robot.ino
@@ -2,18 +2,34 @@
 #include <Servo.h>
 #include <ESP8266WiFi.h>                // Include main library
 
-#define SERVO_PIN D2  // นี่คือกำหนดขาให้ Servo
+//#define SERVO_PIN D2  // นี่คือกำหนดขาให้ Servo
 
 const char* ssid     = "shareEWTC";     // Set SSID
 const char* password = "12345abcde"; // Set password
 const char* host = "dweet.io";          // Set host
 
-Servo myservo;  // ประกาศ object ของการใช้ Servo
+String line;
+String test;
+String strServo;
+
+//int intStart[4] = {129, 120, 103, 102};
+
+//Servo myservo;  // ประกาศ object ของการใช้ Servo
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
 int analogValue;
 
 void setup()
 {
-  myservo.attach(SERVO_PIN);
+  //myservo.attach(SERVO_PIN);
+  servo1.attach(4);
+  servo2.attach(0);
+  servo3.attach(2);
+  servo4.attach(14);
+
   Serial.begin(115200);                 // Print setting message
   delay(10);
   Serial.println();
@@ -47,25 +63,72 @@ void loop()
                "Host: " + host + "\r\n" +
                "Connection: keep-alive\r\n" +
                "Cache-Control: max-age=0\r\n\r\n");
-  delay(1000);
+  delay(3000);
+
   while (client.available())
   {
-    String line = client.readStringUntil('\r');
-    Serial.println(line);
+    line = client.readStringUntil('\r'); //this full word ***
 
-    String test = line.substring(134, 137);
-    Serial.println(test);
+    test = line.substring(134, 137); //cut word for value **
+
+    strServo = line.substring(131, 132);
 
     analogValue = test.toInt(); // Change String to int
-    //analogValue = map(analogValue, 0, 1023, 0, 140);
 
-    Serial.println();
-    Serial.println();
-    Serial.print("andlogValue ==> ");
-    Serial.println(analogValue);
-
-    myservo.write(analogValue);
-
+    //set deg of servo (val, start, 0 deg, end deg)
+    analogValue = map(analogValue, 0, 100, 129, 154);
 
   } // while
+
+  Serial.println("Full word = " + line);
+  Serial.println();
+  Serial.println("Server Value = " + test);
+  Serial.println();
+  Serial.println("Servo No. = " + strServo);
+  Serial.println();
+  Serial.print("andlogValue ==> ");
+  Serial.println(analogValue);
+  Serial.println();
+  Serial.println();
+
+  //output to servo
+  //myservo.write(analogValue);
+
+  switch (strServo.toInt()) {
+    case 1:
+      Serial.println("Output to Servo 1");
+      Serial.println();
+      servo1.write(analogValue);
+      break;
+      
+    case 2:
+      Serial.println("Output to Servo 2");
+      Serial.println();
+      servo2.write(analogValue);
+      break;
+      
+    case 3:
+      Serial.println("Output to Servo 3");
+      Serial.println();
+      servo3.write(analogValue);
+      break;
+      
+    case 4:
+      Serial.println("Output to Servo 4");
+      Serial.println();
+      servo4.write(analogValue);
+      break;
+      
+    default:
+      // statements
+      break;
+  }
+
 } // loop
+
+
+
+
+
+
+
